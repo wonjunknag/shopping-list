68d7df31e928abb38429683ef0557c1193fdc2a6
wonjunkang <kwjcomein@gmail.com>
2020-08-05 21:28:34 +0900

    JS: 기능 구현

---
+++ js/main.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const items = document.querySelector(".items");
+const input = document.querySelector(".footer__input");
+const addBtn = document.querySelector(".footer__button");
+
+function onAdd() {
+  // 1. 사용자가 입력한 텍스트를 받아옴
+  const text = input.value;
+  if (text === "") {
+    input.focus();
+    return;
+  }
+  // 2. 새로운 아이템을 만듬(텍스트 + 삭제버튼)
+  const item = createItem(text);
+  // 3. items 컨테이너안에 새로 만든 아이템을 추가한다.
+  items.appendChild(item);
+  // 4. 새로 추가된 아이템으로 스크롤링
+  item.scrollIntoView({ block: "center" });
+  // 5. 인풋을 초기화 한다.
+  input.value = "";
+  input.focus();
+}
+function createItem(text) {
+  const itemsRow = document.createElement("li");
+  itemsRow.setAttribute("class", "item__row");
+
+  const item = document.createElement("div");
+  item.setAttribute("class", "item");
+
+  const name = document.createElement("span");
+  name.setAttribute("class", "item__name");
+  name.textContent = text;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.setAttribute("class", "item__delete");
+  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  deleteBtn.addEventListener("click", () => {
+    items.removeChild(itemsRow);
+  });
+
+  const itemDivider = document.createElement("div");
+  itemDivider.setAttribute("class", "item__divider");
+
+  item.appendChild(name);
+  item.appendChild(deleteBtn);
+
+  itemsRow.appendChild(item);
+  itemsRow.appendChild(itemDivider);
+  return itemsRow;
+}
+addBtn.addEventListener("click", () => {
+  onAdd();
+});
+
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    onAdd();
+  }
+});
