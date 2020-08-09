567858ad8205e8be3ce7ac1e4fc0b26c13063abc
wonjunkang <kwjcomein@gmail.com>
2020-08-09 16:09:15 +0900

    JS: modified

--- js/main.js
+++ js/main.js
@@ -4,49 +4,36 @@ const items = document.querySelector(".items");
 const input = document.querySelector(".footer__input");
 const addBtn = document.querySelector(".footer__button");
 
+/**
+ *
+ */
 function onAdd() {
-  // 1. 사용자가 입력한 텍스트를 받아옴
   const text = input.value;
   if (text === "") {
     input.focus();
     return;
   }
-  // 2. 새로운 아이템을 만듬(텍스트 + 삭제버튼)
   const item = createItem(text);
-  // 3. items 컨테이너안에 새로 만든 아이템을 추가한다.
   items.appendChild(item);
-  // 4. 새로 추가된 아이템으로 스크롤링
   item.scrollIntoView({ block: "center" });
-  // 5. 인풋을 초기화 한다.
   input.value = "";
   input.focus();
 }
+let id = 0; // UUID
 function createItem(text) {
   const itemsRow = document.createElement("li");
   itemsRow.setAttribute("class", "item__row");
-
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
-
-  const name = document.createElement("span");
-  name.setAttribute("class", "item__name");
-  name.textContent = text;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item__delete");
-  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  deleteBtn.addEventListener("click", () => {
-    items.removeChild(itemsRow);
-  });
-
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item__divider");
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemsRow.appendChild(item);
-  itemsRow.appendChild(itemDivider);
+  itemsRow.setAttribute("data-id", `${id}`);
+  itemsRow.innerHTML = `  
+      <div class="item">
+         <span class="item__name">${text}</span>
+         <button class="item__delete">
+           <i class="fas fa-trash-alt" data-id=${id}></i>
+         </button>
+      </div>
+      <div class="item__divider"></div>
+  `;
+  id++;
   return itemsRow;
 }
 addBtn.addEventListener("click", () => {
@@ -56,5 +43,14 @@ addBtn.addEventListener("click", () => {
 input.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     onAdd();
+  }
+});
+
+items.addEventListener("click", (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    console.log(toBeDeleted);
+    toBeDeleted.remove();
   }
 });
